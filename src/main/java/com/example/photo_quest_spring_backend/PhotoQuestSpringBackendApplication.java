3964ec35397b00c87047d9bd328969707547b261
Erik Gustafsson <erikgust2@gmail.com>
2022-04-27 09:38:21 +0200

    Initial commit

---
+++ src/main/java/com/example/photo_quest_spring_backend/PhotoQuestSpringBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.photo_quest_spring_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoQuestSpringBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhotoQuestSpringBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/photo_quest_spring_backend/PhotoQuestSpringBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.photo_quest_spring_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoQuestSpringBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
